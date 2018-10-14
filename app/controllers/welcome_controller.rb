d23b22cc931f46078ab152f8e58d025bddff991a
oggs22 <adolfoyanes@gmail.com>
2018-10-14 13:22:02 -0500

    welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,5 +6,31 @@ class WelcomeController < ApplicationController
 	def ebay_accept
 		puts "entramos en ebay_accept"
 		puts params 
+		code = Base64.decode64(params["code"])  
+		puts "el code es"
+		puts code
+
+
+		client_id = ENV.fetch('CLIENT_ID')
+
+		cert_id = ENV.fetch('CERT_ID')
+
+		authorization = Base64.encode64("#{client_id}:cert_id")
+		authorization = authorization[0..(authorization.size-2)]
+
+		puts "el authorization es"
+
+		url_base = "https://api.ebay.com/identity/v1/oauth2/token"
+
+		header    = {"Content-Type" => "application/x-www-form-urlencoded" ,
+			"Authorization" => "Basic #{authorization}"}
+
+		parametros = {"grant_type"=>"authorization_code", "code"=>code, "redirect_uri" =>"<%= url_for token_path %> "}
+
+		response = HTTParty.post("#{url_base}", :query => parametros, :headers =>header)
+
+		res = JSON.parse response.body 
+
+
 	end
 end
--- taller_ebay.rb
+++ taller_ebay.rb
@@ -3,13 +3,12 @@ taller_ebay.rb
 
 ### obtener token 
 
-code = Base64.encode64(params["code"])    
-code = code[0..(code.size-2)]              
+code = Base64.decode64(params["code"])    
+# code = code[0..(code.size-2)]              
         
 client_id = ENV.fetch('CLIENT_ID')
 
 cert_id = ENV.fetch('CERT_ID')
-
 
 authorization = Base64.encode64("#{client_id}:cert_id")
 authorization = authorization[0..(authorization.size-2)]
@@ -17,13 +16,9 @@ authorization = authorization[0..(authorization.size-2)]
 url_base = "https://api.ebay.com/identity/v1/oauth2/token"
 
 header    = {"Content-Type" => "application/x-www-form-urlencoded" ,
-                     "Authorization" = "Basic #{authorization}"}
+	"Authorization" => "Basic #{authorization}"}
 
-parametros = {
-				"grant_type"=>"authorization_code", 
-				"code"=>code, 
-				"redirect_uri" =>"<%= url_for token_path %>"
-				}
+parametros = {"grant_type"=>"authorization_code", "code"=>code, "redirect_uri" =>"https://ebayapp.herokuapp.com/token"}
 
 response = HTTParty.post("#{url_base}", :query => parametros, :headers =>header)
 
