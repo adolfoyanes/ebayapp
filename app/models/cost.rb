2063ed2ec232313c3cc646562fd549405065e3be
oggs22 <adolfoyanes@gmail.com>
2018-11-02 08:09:01 -0600

    fix

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -3,16 +3,17 @@ class Cost < ApplicationRecord
 	def self.import(file)
     CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
     	puts row
-    	# cost = Cost.new
-    	# cost.brand = row["brand"]
-    	# cost.brand = row["category"]
-    	# cost.brand = row["modelo"]
-    	# cost.brand = row["color"]
-    	# cost.brand = row["medida"]
-    	# cost.brand = row["upc"]
-    	# cost.brand = row["precio_esp"]
-    	# cost.save 
-      Cost.create! row.to_hash
+    	cost = Cost.new
+    	cost.brand = row["brand"]
+    	cost.category = row["category"]
+    	cost.modelo = row["modelo"]
+    	cost.color = row["color"]
+    	cost.medida = row["medida"]
+    	cost.upc = row["upc"]
+    	cost.precio_esp = row["precio_esp"]
+        cost.frame_color = row["frame_color"]
+    	cost.save 
+      # Cost.create! row.to_hash
     end
   end
 end
